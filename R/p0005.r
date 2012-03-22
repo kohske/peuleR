dbaecbd050a3a0a52b370c65f288bffde46c4a9d
Kohske Takahashi @ jurina <takahashi.kohske@gmail.com>
2012-03-22 20:07:42 +0900

    update No 5 to use LCM in pracma

--- R/p0005.r
+++ R/p0005.r
@@ -6,5 +6,10 @@ z <- do.call("rbind",
               lapply(2:20, function(i) as.data.frame(table(as.numeric(factorize(i))))))
 a <- prod(sapply(primes(20), function(i) i^max(subset(z, Var1 == i)$Freq)))
 
+# another solution
+b <- Reduce(function(a, b) LCM(a,b), 1:20, 1)
+
+print(identical(a, b))
+
 print(a)
 cat(a, file = pipe('pbcopy'))
