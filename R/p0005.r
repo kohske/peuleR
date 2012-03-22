28acadb8b1736fd90bc33ee30a7bd2a38558145f
Kohske Takahashi @ jurina <takahashi.kohske@gmail.com>
2012-03-22 19:56:39 +0900

    No 5 solved. very fast.

---
+++ R/p0005.r
@@ -0,0 +1,10 @@
+# http://projecteuler.net/problem=5
+
+library(pracma)
+
+z <- do.call("rbind",
+              lapply(2:20, function(i) as.data.frame(table(as.numeric(factorize(i))))))
+a <- prod(sapply(primes(20), function(i) i^max(subset(z, Var1 == i)$Freq)))
+
+print(a)
+cat(a, file = pipe('pbcopy'))
